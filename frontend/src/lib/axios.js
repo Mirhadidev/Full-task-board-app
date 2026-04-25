4fbf1cd4e26acbf11f00b406d0536b46267f4753
Mir Hadi ali <mirhadiali62@gmail.com>
2026-04-25 18:04:32 +0500

    fix api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 console.log("API URL:", import.meta.env.VITE_API_URL); // 👈 ADD THIS
 const api = axios.create({
-  baseURL:`${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default api;
